e61563192df4f4cb78cfdc9671d0b4433f212b3e
SyncMr <talk2pragadheesh@gmail.com>
2016-08-16 09:13:59 -0700

    Adding java

--- Coursera/2016_2_DataStructuresInJava/MOOCTextEditor/src/document/EfficientDocument.java
+++ Coursera/2016_2_DataStructuresInJava/MOOCTextEditor/src/document/EfficientDocument.java
@@ -19,6 +19,9 @@ public class EfficientDocument extends Document {
 	{
 		super(text);
 		processText();
+		numWords = 0;
+		numSentences = 0;
+		numSyllables = 0;
 	}
 	
 	
@@ -47,11 +50,19 @@ public class EfficientDocument extends Document {
 	{
 		// Call getTokens on the text to preserve separate strings that are 
 		// either words or sentence-ending punctuation.  Ignore everything
-		// That is not a word or a sentence-ending puctuation.
+		// That is not a word or a sentence-ending punctuation.
+		
 		// MAKE SURE YOU UNDERSTAND THIS LINE BEFORE YOU CODE THE REST
 		// OF THIS METHOD.
 		List<String> tokens = getTokens("[!?.]+|[a-zA-Z]+");
 		
+		System.out.println("Text: " + getText());
+		for (String str : tokens)
+		{
+			System.out.println("Token: " + str);
+			numSyllables += countSyllables(str);
+			if ()
+		}
 		// TODO: Finish this method.  Remember the countSyllables method from 
 		// Document.  That will come in handy here.  isWord defined above will also help.
 	}
@@ -123,23 +134,26 @@ public class EfficientDocument extends Document {
 	// We encourage you to add your own tests here.
 	public static void main(String[] args)
 	{
+		testCase(new EfficientDocument("Here is a series of test sentences. Your program should "
+				+ "find 3 sentences, 33 words, and 49 syllables. Not every word will have "
+				+ "the correct amount of syllables (example, for example), "
+				+ "but most of them will."), 49, 33, 3);
+		/*
 	    testCase(new EfficientDocument("This is a test.  How many???  "
                 + "Senteeeeeeeeeences are here... there should be 5!  Right?"),
                 16, 13, 5);
+	    
         testCase(new EfficientDocument(""), 0, 0, 0);
         testCase(new EfficientDocument("sentence, with, lots, of, commas.!  "
                 + "(And some poaren)).  The output is: 7.5."), 15, 11, 4);
         testCase(new EfficientDocument("many???  Senteeeeeeeeeences are"), 6, 3, 2); 
-        testCase(new EfficientDocument("Here is a series of test sentences. Your program should "
-				+ "find 3 sentences, 33 words, and 49 syllables. Not every word will have "
-				+ "the correct amount of syllables (example, for example), "
-				+ "but most of them will."), 49, 33, 3);
+        
 		testCase(new EfficientDocument("Segue"), 2, 1, 1);
 		testCase(new EfficientDocument("Sentence"), 2, 1, 1);
 		testCase(new EfficientDocument("Sentences?!"), 3, 1, 1);
 		testCase(new EfficientDocument("Lorem ipsum dolor sit amet, qui ex choro quodsi moderatius, nam dolores explicari forensibus ad."),
 		         32, 15, 1);
-		
+		*/
 	}
 	
 
