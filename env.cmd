11e87c248a1394fc344eebfbf4e880619000c6ff
SyncMr <talk2pragadheesh@gmail.com>
2016-10-05 08:07:32 -0700

    cmd prompt alias tips

---
+++ env.cmd
@@ -0,0 +1,10 @@
+doskey np="C:\Program Files (x86)\Notepad++\notepad++.exe" $*
+doskey cdpr=cd "C:\PRAGADHE"
+doskey cdde=cd "C:\PRAGADHE\DEV_GIT"
+doskey cdal=cd "C:\PRAGADHE\DEV_GIT\Algos"
+doskey cdcp=cd "C:\PRAGADHE\DEV_GIT\LearnCpp"
+doskey cdja=cd "C:\PRAGADHE\DEV_GIT\LearnJava"
+doskey cdpy=cd "C:\PRAGADHE\DEV_GIT\LearnPython"
+doskey cdot=cd "C:\PRAGADHE\DEV_GIT\OtherLearnings"
+doskey mkcp=g++ -std=gnu++11 $*
+cls
